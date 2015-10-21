edf7332126d66973a0f64e8e65f0df44c24e30be
Shubham Kedia <Shubham@Shubhams-MacBook.local>
2015-10-20 22:45:53 -0700

    Test on the watch.

--- AceSATWatch Extension/TestInterfaceController.swift
+++ AceSATWatch Extension/TestInterfaceController.swift
@@ -12,17 +12,30 @@ import Foundation
 
 class TestInterfaceController: WKInterfaceController {
     
-
-    @IBOutlet var definitionLabel1: WKInterfaceButton!
-    @IBOutlet var definitionLabel2: WKInterfaceButton!
-    @IBOutlet var definitionLabel3: WKInterfaceButton!
-    @IBOutlet var definitionLabel4: WKInterfaceButton!
+    @IBOutlet var definition1Button: WKInterfaceButton!
+    @IBOutlet var definition2Button: WKInterfaceButton!
+    @IBOutlet var definition3Button: WKInterfaceButton!
+    @IBOutlet var definition4Button: WKInterfaceButton!
     
-    var buttons = [WKInterfaceButton]()
+    @IBOutlet var definition1SelectedImage: WKInterfaceImage!
+    @IBOutlet var definition2SelectedImage: WKInterfaceImage!
+    @IBOutlet var definition3SelectedImage: WKInterfaceImage!
+    @IBOutlet var definition4SelectedImage: WKInterfaceImage!
     
-    var testQuestion: TestQuestion?
+    @IBOutlet var definition1Label: WKInterfaceLabel!
+    @IBOutlet var definition2Label: WKInterfaceLabel!
+    @IBOutlet var definition3Label: WKInterfaceLabel!
+    @IBOutlet var definition4Label: WKInterfaceLabel!
     
     @IBOutlet var wordLabel: WKInterfaceLabel!
+    
+    var definitionButtons = [WKInterfaceButton]()
+    var definitionSelectedImages = [WKInterfaceImage]()
+    var definitionLabels = [WKInterfaceLabel]()
+    
+    var presentedDefinitions = [String]()
+    
+    var testQuestion: TestQuestion?
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
@@ -30,17 +43,49 @@ class TestInterfaceController: WKInterfaceController {
         wordLabel.setTextColor(UIColor.ace_redColor())
         wordLabel.setText(testQuestion?.word.word)
         
-        buttons = [definitionLabel1, definitionLabel2, definitionLabel3, definitionLabel4]
-        var definitions = testQuestion!.possibleDefinitions
-        definitions.insert(testQuestion!.word.definition, atIndex: random() % 4)
-        for i in 0..<definitions.count {
-            buttons[i].setTitle(definitions[i])
+        definitionButtons = [definition1Button, definition2Button, definition3Button, definition4Button]
+        definitionSelectedImages = [definition1SelectedImage, definition2SelectedImage, definition3SelectedImage, definition4SelectedImage]
+        definitionLabels = [definition1Label, definition2Label, definition3Label, definition4Label]
+
+        presentedDefinitions = testQuestion!.possibleDefinitions
+        presentedDefinitions.insert(testQuestion!.word.definition, atIndex: random() % 4)
+        for i in 0..<presentedDefinitions.count {
+            definitionLabels[i].setText(presentedDefinitions[i])
+            definitionSelectedImages[i].setTintColor(UIColor(white: 0.3, alpha: 1.0))
         }
     }
 
-    @IBAction func definitionTapped(sender: WKInterfaceButton) {
-        sender.setBackgroundColor(UIColor.ace_redColor())
-        
+    func updateButtonStates() {
+        let selectedIndex = testQuestion!.userSelectedDefinition != nil ? presentedDefinitions.indexOf(testQuestion!.userSelectedDefinition!) : -1
+        for i in 0..<definitionButtons.count {
+            if i == selectedIndex {
+                definitionLabels[i].setTextColor(UIColor.ace_redColor())
+                definitionSelectedImages[i].setTintColor(UIColor.ace_redColor())
+            }
+            else {
+                definitionLabels[i].setTextColor(UIColor.whiteColor())
+                definitionSelectedImages[i].setTintColor(UIColor(white: 0.3, alpha: 1.0))
+            }
+        }
+    }
+    
+    @IBAction func definition1ButtonTapped() {
+        testQuestion!.userSelectedDefinition = presentedDefinitions[0]
+        updateButtonStates()
+    }
+    
+    @IBAction func definition2ButtonTapped() {
+        testQuestion!.userSelectedDefinition = presentedDefinitions[1]
+        updateButtonStates()
     }
 
+    @IBAction func definition3ButtonTapped() {
+        testQuestion!.userSelectedDefinition = presentedDefinitions[2]
+        updateButtonStates()
+    }
+    
+    @IBAction func definition4ButtonTapped() {
+        testQuestion!.userSelectedDefinition = presentedDefinitions[3]
+        updateButtonStates()
+    }
 }
